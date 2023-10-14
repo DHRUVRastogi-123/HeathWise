b747989912fd59f80bc9bb108bed59b30f4fb443
M. Farhaan baig <116679001+fourhaan@users.noreply.github.com>
2023-10-14 12:52:44 +0530

    Add files via upload

---
+++ ReactApp/src/components/Drug-Description/Precaution.jsx
@@ -0,0 +1,13 @@
+function Precaution(){
+    return <div  id ="precaution" className="uses-container">
+        <div className="uses-heading">
+        <h2>Precaution</h2>
+        </div>
+       <div className="uses-description">
+       <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestias similique autem cum adipisci placeat magni nobis aspernatur perferendis facilis fugiat vitae repudiandae, ex quo amet natus in, numquam explicabo quod magnam? Possimus ad consequuntur natus quasi nobis quibusdam voluptas suscipit officiis similique harum inventore quas ipsa porro est veritatis, voluptates facilis. Nisi eaque assumenda voluptates delectus accusantium quas error repellendus omnis, aperiam in recusandae earum iste optio quam alias sequi exercitationem iure incidunt. Eligendi adipisci amet est distinctio doloremque, ut autem at nemo mollitia, similique nisi pariatur provident expedita veniam iusto sit neque praesentium. Expedita consequatur ipsum tempora vero quam?</p>
+        <hr></hr>
+       </div>
+    </div>
+    }
+
+export default Precaution;
